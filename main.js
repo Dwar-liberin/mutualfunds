73e35d678dfe64bdc2f1ba0855c1daf1e7c966bb
Liberin <165782640+Dwar-liberin@users.noreply.github.com>
2024-06-19 10:44:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@
               appName: "DwAR",
               customerId: "4",
               campaignName: "Mutual Fund",
-              serverUrl: "https://3.110.158.27/event"
+              serverUrl: "https://staging.lttl.in/event"
           }
        );
 
@@ -152,68 +152,68 @@
 
   
     
-      const target_imagef875af5cad2_iconGeometry = new THREE.PlaneGeometry(1, 0.6346691519105312);
-   const target_imagef875af5cad2_texture = await loadTexture("assets/mutual-fund-marker.jpg");
-  const target_imagef875af5cad2_image = new THREE.MeshBasicMaterial({
-      map: target_imagef875af5cad2_texture,
+      const image_c938f518_d34d15d8_iconGeometry = new THREE.PlaneGeometry(1, 0.21);
+   const image_c938f518_d34d15d8_texture = await loadTexture("assets/mutual-fund-cta.png");
+  const image_c938f518_d34d15d8_image = new THREE.MeshBasicMaterial({
+      map: image_c938f518_d34d15d8_texture,
     });
-    const target_imagef875af5cad2 = new THREE.Mesh(target_imagef875af5cad2_iconGeometry, target_imagef875af5cad2_image);
-    target_imagef875af5cad2.scale.set(1, 1, 1);
-    target_imagef875af5cad2.position.set(0, 0, 0);
-    target_imagef875af5cad2.rotation.set(-0.001, 0, 0);
+    const image_c938f518_d34d15d8 = new THREE.Mesh(image_c938f518_d34d15d8_iconGeometry, image_c938f518_d34d15d8_image);
+    image_c938f518_d34d15d8.scale.set(0.5, 0.5, 0.5);
+    image_c938f518_d34d15d8.position.set(0, -0.448, 0.04);
+    image_c938f518_d34d15d8.rotation.set(-0.001, 0, 0);
+    image_c938f518_d34d15d8.userData.clickable = true
+    
+    image_c938f518_d34d15d8.userData.eventName ="Mutual Fund Website"
+const target_imageundefi4025d_iconGeometry = new THREE.PlaneGeometry(1, 0.6346691519105312);
+   const target_imageundefi4025d_texture = await loadTexture("assets/mutual-fund-marker.jpg");
+  const target_imageundefi4025d_image = new THREE.MeshBasicMaterial({
+      map: target_imageundefi4025d_texture,
+    });
+    const target_imageundefi4025d = new THREE.Mesh(target_imageundefi4025d_iconGeometry, target_imageundefi4025d_image);
+    target_imageundefi4025d.scale.set(1, 1, 1);
+    target_imageundefi4025d.position.set(0.01, -0.01, 0.01);
+    target_imageundefi4025d.rotation.set(-0.001, 0, 0);
     
     
     
 
-    const video_asset_3ba021c08ae_planeGeometry = new THREE.PlaneGeometry(1, 0.5625);
+    const video_asset_3ba021dccfb_planeGeometry = new THREE.PlaneGeometry(1, 0.5625);
 
-    const video_asset_3ba021c08ae_Item0Video = await loadVideo("assets/mutual-funds-video.mp4");
+    const video_asset_3ba021dccfb_Item0Video = await loadVideo("assets/mutual-funds-video.mp4");
 
-    const video_asset_3ba021c08ae_Item0VideoTexture = new THREE.VideoTexture(
-      video_asset_3ba021c08ae_Item0Video
+    const video_asset_3ba021dccfb_Item0VideoTexture = new THREE.VideoTexture(
+      video_asset_3ba021dccfb_Item0Video
     );
 
-    let video_asset_3ba021c08ae_Item0VideoMaterial
+    let video_asset_3ba021dccfb_Item0VideoMaterial
 
-      video_asset_3ba021c08ae_Item0VideoMaterial = new THREE.MeshBasicMaterial({
-          map: video_asset_3ba021c08ae_Item0VideoTexture,
+      video_asset_3ba021dccfb_Item0VideoMaterial = new THREE.MeshBasicMaterial({
+          map: video_asset_3ba021dccfb_Item0VideoTexture,
         })
     
-     const video_asset_3ba021c08ae = new THREE.Mesh(
-      video_asset_3ba021c08ae_planeGeometry,
-      video_asset_3ba021c08ae_Item0VideoMaterial
+     const video_asset_3ba021dccfb = new THREE.Mesh(
+      video_asset_3ba021dccfb_planeGeometry,
+      video_asset_3ba021dccfb_Item0VideoMaterial
     );
 
-  video_asset_3ba021c08ae.position.set(0, 0, 0);
+  video_asset_3ba021dccfb.position.set(0, 0, 0);
 
 
 
   if (isIOS) {
-    video_asset_3ba021c08ae_Item0Video.muted=isIOS
+    video_asset_3ba021dccfb_Item0Video.muted=isIOS
     muteIconMesh = await loadUnmuteLogo();
     anchor.group.add(muteIconMesh);
   }
 
-  video_asset_3ba021c08ae_Item0Video.loop=true;
+  video_asset_3ba021dccfb_Item0Video.loop=true;
   
-  video_asset_3ba021c08ae.scale.set(1, 1, 1);
+  video_asset_3ba021dccfb.scale.set(1, 1, 1);
 
-    video_asset_3ba021c08ae.rotation.set(-0.001, 0, 0);
+    video_asset_3ba021dccfb.rotation.set(-0.002, 0, 0);
 
     
   
-const image_c938f518_d34d34db_iconGeometry = new THREE.PlaneGeometry(1, 0.21);
-   const image_c938f518_d34d34db_texture = await loadTexture("assets/mutual-fund-cta.png");
-  const image_c938f518_d34d34db_image = new THREE.MeshBasicMaterial({
-      map: image_c938f518_d34d34db_texture,
-    });
-    const image_c938f518_d34d34db = new THREE.Mesh(image_c938f518_d34d34db_iconGeometry, image_c938f518_d34d34db_image);
-    image_c938f518_d34d34db.scale.set(0.5, 0.5, 0.5);
-    image_c938f518_d34d34db.position.set(0, -0.4, 0);
-    image_c938f518_d34d34db.rotation.set(-0.001, 0, 0);
-    image_c938f518_d34d34db.userData.clickable = true
-    
-    image_c938f518_d34d34db.userData.eventName ="Mutual Fund"
       
        document.body.addEventListener("click", (e) => {
     const mouseX = (e.clientX / window.innerWidth) * 2 - 1;
@@ -246,7 +246,7 @@ const image_c938f518_d34d34db_iconGeometry = new THREE.PlaneGeometry(1, 0.21);
 
         if(isIOS){ 
           if (o.userData.clickable && o === muteIconMesh) {
-            video_asset_3ba021c08ae_Item0Video.muted=false
+            video_asset_3ba021dccfb_Item0Video.muted=false
     
             anchor.group.remove(muteIconMesh);
             return true;
@@ -259,7 +259,7 @@ const image_c938f518_d34d34db_iconGeometry = new THREE.PlaneGeometry(1, 0.21);
       if (o.userData.clickable) window.showLoadingScreen();
 
       
-      if (o.userData.clickable && o === image_c938f518_d34d34db) {
+      if (o.userData.clickable && o === image_c938f518_d34d15d8) {
         setTimeout(()=>{
           window.location.href = "https://www.mutualfundssahihai.com/en"
         },100)
@@ -270,9 +270,9 @@ const image_c938f518_d34d34db_iconGeometry = new THREE.PlaneGeometry(1, 0.21);
     })
     
       
-    
-anchor.group.add(video_asset_3ba021c08ae)
-anchor.group.add(image_c938f518_d34d34db)
+    anchor.group.add(image_c938f518_d34d15d8)
+
+anchor.group.add(video_asset_3ba021dccfb)
 
 
     anchor.onTargetFound = () => {
@@ -289,12 +289,12 @@ anchor.group.add(image_c938f518_d34d34db)
 
 
      
-      video_asset_3ba021c08ae_Item0Video.play();
+      video_asset_3ba021dccfb_Item0Video.play();
     };
 
 
     anchor.onTargetLost = () => {
-       video_asset_3ba021c08ae_Item0Video.pause();
+       video_asset_3ba021dccfb_Item0Video.pause();
 
         
 
